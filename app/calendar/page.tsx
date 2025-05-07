b422f65c215109edb1f4374df20bb757c3b828cc
hikaru-0602 <beretta0602@gmail.com>
2025-05-07 16:46:53 +0900

    修正

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Header from "../components/Header";
+import Header from "../components/header";
 import Calendar from "./Calendar";
 
 export default function Home() {
