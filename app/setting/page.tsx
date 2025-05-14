9fed893fa972b9d223ca296af4ccc8888190b921
hikaru-0602 <beretta0602@gmail.com>
2025-05-14 10:58:39 +0900

    ログインボタン修正

--- app/setting/page.tsx
+++ app/setting/page.tsx
@@ -72,7 +72,7 @@ export default function Work() {
               onClick={signIn}
               className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
             >
-              ログイン
+              学内アカウントログイン
             </button>
           )}
           {user && (
