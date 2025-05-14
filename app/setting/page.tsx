173b02b0e53aa6457fc57a8ff9140a8dbc02bbd8
hikaru-0602 <beretta0602@gmail.com>
2025-05-14 10:51:23 +0900

    alert追加

--- app/setting/page.tsx
+++ app/setting/page.tsx
@@ -216,6 +216,7 @@ export default function Work() {
                     alert("入力に不備があります。");
                     return;
                   }
+                  alert("登録しました。");
                   handleUserRegister();
                 }}
                 className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
