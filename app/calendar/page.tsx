e232f2f685875662019d49f3ec7bd14d6dc6e6b5
hikaru-0602 <beretta0602@gmail.com>
2025-05-14 12:11:52 +0900

    修正

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -7,7 +7,10 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className="flex flex-col items-center justify-center min-h-screen p-8 mt-16">
+      <div className="flex flex-col items-center justify-center min-h-screen p-8">
+        <h1 className="text-2xl font-bold text-center mt-20">
+          パソコンでの利用がおすすめ
+        </h1>
         <Calendar />
       </div>
     </>
